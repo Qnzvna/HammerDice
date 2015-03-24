38004dbf07bf19433da5af8c3abe0b08f6ab8898
Damian Serwin <damian.abel.serwin@gmail.com>
2015-03-24 23:12:51 +0100

    new version specially for play google store init

--- app/src/main/java/tk/owab/hammerdice/MainActivity.java
+++ app/src/main/java/tk/owab/hammerdice/MainActivity.java
@@ -4,11 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,18 +13,8 @@ import android.widget.ToggleButton;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URI;
 
 public class MainActivity extends Activity {
 
@@ -35,7 +22,6 @@ public class MainActivity extends Activity {
     public final static String HIT = "tk.owab.hammerdice.HIT";
     public final static String WOUND = "tk.owab.hammerdice.WOUND";
     public final static String ASAVE = "tk.owab.hammerdice.ASAVE";
-    public final static int buildVersion = 0;
 
     private int dices = 1;
     private double chanceToHit = 1;
@@ -55,7 +41,7 @@ public class MainActivity extends Activity {
 
         Tracker t = ((AnalyticsApp) getApplication()).getTracker(AnalyticsApp.TrackerName.APP_TRACKER);
 
-        t.setScreenName("tk.owab.hammerdice.MainActivity");
+        t.setScreenName("main");
 
         // Send a screen view.
         t.send(new HitBuilders.ScreenViewBuilder().build());
